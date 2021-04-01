484c6194879f65b9d4fb9895002d5652af72deaf
Luke Oloffs <luke@Lukes-MacBook-Air.local>
2021-04-01 15:32:50 -0700

    http querystrings

--- demo_http.js
+++ demo_http.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 // create server object
 http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'}); // header
   res.write('Hello World!'); // write a response to the client
   res.end(); // end the response
 }).listen(8080); // the server object listens on port 8080

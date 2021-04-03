c77be640df7baab0cd493adbdc2d73a328cafae5
Luke Oloffs <luke@Lukes-MacBook-Air.local>
2021-04-03 15:06:21 -0700

    FS with URL module

--- demo_fileserver.js
+++ demo_fileserver.js
@@ -1,10 +1,18 @@
+const http = require('http');
 const url = require('url');
-const adr = 'http://localhost:8080/default.htm?year=2017&month=february';
-const q = url.parse(adr, true);
+const fs = require('fs');
 
-console.log(q.host); //returns 'localhost:8080'
-console.log(q.pathname); //returns '/default.htm'
-console.log(q.search); //returns '?year=2017&month=february'
 
-const qdata = q.query; //returns an object: { year: 2017, month: 'february' }
-console.log(qdata.month); //returns 'february'
+http.createServer(function(req, res) {
+  const q = url.parse(req.url, true);
+  const filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
